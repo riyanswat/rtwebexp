f51cf4db8945df669cdd60ef50837a8f7c3d6906
Rayan Khan <riyanswat@gmail.com>
2025-09-08 05:39:15 +0000

    ..

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -30,7 +30,7 @@ const Hero = () => {
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
-                    href="/signin"
+                    href="/get-a-quote"
                     className="rounded-xs bg-primary px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
                   >
                     Get a Free Quote
